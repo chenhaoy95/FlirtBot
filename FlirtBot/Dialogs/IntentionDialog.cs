949be365da66707bbab1002b49737f09c53ab72c
ericawu <erica15wu@gmail.com>
2016-07-25 21:01:04 -0700

    intention entity works

--- FlirtBot/Dialogs/IntentionDialog.cs
+++ FlirtBot/Dialogs/IntentionDialog.cs
@@ -8,31 +8,21 @@ using System.Web;
 using System.Threading.Tasks;
 using Microsoft.Bot.Connector;
 using FlirtBot.Domain;
+using Microsoft.Bot.Builder.Luis.Models;
 
 namespace FlirtBot.Dialogs
 {
     [Serializable]
     [LuisModel(Constants.IntentionAppId, Constants.IntentionSubscriptionKey)]
-    public class IntentionDialog : IDialog<object>
+    public class IntentionDialog : LuisDialog<object>
     {
 
-        public async Task StartAsync(IDialogContext context)
+        [LuisIntent("SetIntention")]
+        public async Task SetIntentionAsync(IDialogContext context, LuisResult result)
         {
-            //await context.PostAsync("What's your intent?");
-            //if ()           
-            context.Wait(SetIntentionAsync);
-            //await context.PostAsync("what's happeninggggg");
-            //UserInfo.Name = message.From.Name;
-        }
-
-        public async Task SetIntentionAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
-        {
-            var message = await argument;
-            //15 is length of //set_intention
-            //var messageText = message.Text.Substring(15);
-
-            await context.PostAsync(message.Text);
-            context.Wait(SetIntentionAsync);
+            await context.PostAsync(result.Entities[0].Type);
+            context.Done(0);
+            //context.Wait(SetIntentionAsync);
         }
 
     }
