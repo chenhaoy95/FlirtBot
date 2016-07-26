d8ed675528864ce2a5894acea0f929ca1dfd34c3
chenhaoy95 <chenhaoy@andrew.cmu.edu>
2016-07-25 20:16:46 -0700

    Add Gender Dialog Class

---
+++ FlirtBot/Dialogs/GenderDialog.cs
@@ -0,0 +1,33 @@
+﻿using FlirtBot.Resources;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+using FlirtBot.Domain;
+
+namespace FlirtBot.Dialogs
+{
+    [Serializable]
+    [LuisModel(Constants.GenderAppId, Constants.GenderSubscriptionKey)]
+    public class GenderDialog : IDialog<object>
+    {
+        private UserInfo UInfo;
+        public GenderDialog(UserInfo uinfo)
+        {
+            UInfo = uinfo;
+        }
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            //await context.PostAsync("What's your intent?");
+            //if ()
+            await context.PostAsync("What's your Gender?");
+            //await context.PostAsync("what's happeninggggg");
+            //UserInfo.Name = message.From.Name;
+        }
+    }
+}
--- FlirtBot/Resources/Constants.cs
+++ FlirtBot/Resources/Constants.cs
@@ -7,7 +7,9 @@ namespace FlirtBot.Resources
 {
     public class Constants
     {
-        public const string AppId = "054d47a2-2886-468a-8412-89fab2b6d10f";
-        public const string SubscriptionKey = "8010b0ad1fa34da798d444a2bc976739";
+        public const string IntentionAppId = "054d47a2-2886-468a-8412-89fab2b6d10f";
+        public const string IntentionSubscriptionKey = "8010b0ad1fa34da798d444a2bc976739";
+        public const string GenderAppId = "9a48f319-843c-4e3e-a94a-03ce5223b484";
+        public const string GenderSubscriptionKey = "071c81f732164290be51c5ad834ad85a";
     }
 }
