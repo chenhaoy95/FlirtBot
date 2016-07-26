7cab7bcd106f2c98838eb52e148eb75b9aec5fe7
ericawu <erica15wu@gmail.com>
2016-07-25 20:25:41 -0700

    intention controller working

--- FlirtBot/Dialogs/IntentionDialog.cs
+++ FlirtBot/Dialogs/IntentionDialog.cs
@@ -12,15 +12,14 @@ using FlirtBot.Domain;
 namespace FlirtBot.Dialogs
 {
     [Serializable]
-    [LuisModel(Constants.AppId, Constants.SubscriptionKey)]
+    [LuisModel(Constants.IntentionAppId, Constants.IntentionSubscriptionKey)]
     public class IntentionDialog : IDialog<object>
     {
 
         public async Task StartAsync(IDialogContext context)
         {
             //await context.PostAsync("What's your intent?");
-            //if ()
-            
+            //if ()           
             context.Wait(SetIntentionAsync);
             //await context.PostAsync("what's happeninggggg");
             //UserInfo.Name = message.From.Name;
@@ -29,10 +28,12 @@ namespace FlirtBot.Dialogs
         public async Task SetIntentionAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
         {
             var message = await argument;
+            //15 is length of //set_intention
+            //var messageText = message.Text.Substring(15);
+
             await context.PostAsync(message.Text);
             context.Wait(SetIntentionAsync);
         }
-
 
     }
 }
